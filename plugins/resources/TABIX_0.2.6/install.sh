b5c9d5bbf4669c83bc93e171a8eaf503638d0224
Fabien Campagne <fac2003@campagnelab.org>
2013-03-28 15:28:12 +0000

    Use quote to prevent shell interpretation of URL.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/campagnelab/gobyweb/trunk/gobyweb_home/conf@34100 60cd2ccc-2b05-0410-9566-da273a940bff

--- plugins/resources/TABIX_0.2.6/install.sh
+++ plugins/resources/TABIX_0.2.6/install.sh
@@ -9,7 +9,7 @@ function plugin_install_artifact {
 
         'BINARIES' )
             VERSION="0.2.6"
-            ${RESOURCES_FETCH_URL_SCRIPT} http://downloads.sourceforge.net/project/samtools/tabix/tabix-0.2.6.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsamtools%2Ffiles%2Ftabix%2F&ts=1364484044&use_mirror=iweb tabix-0.2.6.tar.bz2
+            ${RESOURCES_FETCH_URL_SCRIPT} "http://downloads.sourceforge.net/project/samtools/tabix/tabix-0.2.6.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsamtools%2Ffiles%2Ftabix%2F&ts=1364484044&use_mirror=iweb" tabix-0.2.6.tar.bz2
             bunzip2  tabix-${VERSION}.tar.bz2
             tar -xvf tabix-${VERSION}.tar
             cd tabix-${VERSION}
