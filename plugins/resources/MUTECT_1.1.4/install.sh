2fdeba2071f24cfa6b80a9defeec3bd32843b0d7
Manuele Simi <manuele.simi@campagnelab.org>
2013-04-08 21:31:23 +0000

    Add  RESOURCE/MuTect (1.1.4)
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/campagnelab/gobyweb/trunk/gobyweb_home/conf@34139 60cd2ccc-2b05-0410-9566-da273a940bff

---
+++ plugins/resources/MUTECT_1.1.4/install.sh
@@ -0,0 +1,28 @@
+# Installation script for MuTect version 1.1.4
+function plugin_install_artifact {
+
+    id=$1
+    installation_path=$2
+
+    case ${id} in
+
+
+        'BINARIES' )
+            VERSION="1.1.4"
+            ${RESOURCES_FETCH_URL_SCRIPT} "http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/muTect-1.1.4-bin.zip" muTect-${VERSION}-bin.zip
+            unzip muTect-${VERSION}-bin.zip
+            cp muTect-${VERSION}.jar muTect.jar
+            cp muTect.jar ${installation_path}/
+            cp unzip ${installation_path}/
+            return 0
+            ;;
+
+        *)  echo "Resource artifact id not recognized: "+$id
+            return 99
+            ;;
+
+    esac
+
+    exit 1
+
+}
