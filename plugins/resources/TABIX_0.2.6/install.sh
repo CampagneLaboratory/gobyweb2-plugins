b6ce95e4833874794ef50fb680f735c10346b27b
Manuele Simi <manuele.simi@campagnelab.org>
2013-04-08 21:34:18 +0000

    Fix a wrong comment and use VERSION for the dowloaded file name in  RESOURCE/Tabix (2.0.6)
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/campagnelab/gobyweb/trunk/gobyweb_home/conf@34140 60cd2ccc-2b05-0410-9566-da273a940bff

--- plugins/resources/TABIX_0.2.6/install.sh
+++ plugins/resources/TABIX_0.2.6/install.sh
@@ -1,4 +1,4 @@
-# Installation script for STAR version 2.2.0g
+# Installation script for Tabix version 0.2.6
 function plugin_install_artifact {
 
     id=$1
@@ -9,7 +9,7 @@ function plugin_install_artifact {
 
         'BINARIES' )
             VERSION="0.2.6"
-            ${RESOURCES_FETCH_URL_SCRIPT} "http://downloads.sourceforge.net/project/samtools/tabix/tabix-0.2.6.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsamtools%2Ffiles%2Ftabix%2F&ts=1364484044&use_mirror=iweb" tabix-0.2.6.tar.bz2
+            ${RESOURCES_FETCH_URL_SCRIPT} "http://downloads.sourceforge.net/project/samtools/tabix/tabix-0.2.6.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsamtools%2Ffiles%2Ftabix%2F&ts=1364484044&use_mirror=iweb" tabix-${VERSION}.tar.bz2
             bunzip2  tabix-${VERSION}.tar.bz2
             tar -xvf tabix-${VERSION}.tar
             cd tabix-${VERSION}
