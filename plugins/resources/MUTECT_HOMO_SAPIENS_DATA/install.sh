db7b9f8f08b60bc0c5773ad64f8acae2de27f6a7
Fabien Campagne <fac2003@campagnelab.org>
2013-04-10 21:52:50 +0000

    Copy the files to installation_path.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/campagnelab/gobyweb/trunk/gobyweb_home/conf@34164 60cd2ccc-2b05-0410-9566-da273a940bff

--- plugins/resources/MUTECT_HOMO_SAPIENS_DATA/install.sh
+++ plugins/resources/MUTECT_HOMO_SAPIENS_DATA/install.sh
@@ -8,10 +8,13 @@ function plugin_install_artifact {
 
 
         'DATA' )
-            VERSION="1.1.4"
+
             ${RESOURCES_FETCH_URL_SCRIPT} "http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/b37_cosmic_v54_120711.vcf" b37_cosmic_v54_120711.vcf
+            cp b37_cosmic_v54_120711.vcf ${installation_path}/
             ${RESOURCES_FETCH_URL_SCRIPT} "http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/dbsnp_132_b37.leftAligned.vcf.gz" dbsnp_132_b37.leftAligned.vcf.gz
-            unzip dbsnp_132_b37.leftAligned.vcf.gz
+            gunzip dbsnp_132_b37.leftAligned.vcf.gz
+            cp dbsnp_132_b37.leftAligned.vcf ${installation_path}/
+
             return 0
             ;;
 
