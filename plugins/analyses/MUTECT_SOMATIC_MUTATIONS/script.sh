63f2a00e7503044cf4c784b6d00ac482ff70d950
Manuele Simi <manuele.simi@campagnelab.org>
2013-04-10 21:21:01 +0000

    Various fixes on MUTECT_SOMATIC_MUTATIONS analysis.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/campagnelab/gobyweb/trunk/gobyweb_home/conf@34161 60cd2ccc-2b05-0410-9566-da273a940bff

--- plugins/analyses/MUTECT_SOMATIC_MUTATIONS/script.sh
+++ plugins/analyses/MUTECT_SOMATIC_MUTATIONS/script.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 . ${RESOURCES_GOBY_SHELL_SCRIPT}
 
@@ -11,6 +12,7 @@ function plugin_alignment_analysis_split {
           --number-of-bytes 50000000 \
           --output ${SPLICING_PLAN_RESULT} \
           $*
+
 }
 
 
@@ -38,8 +40,20 @@ function plugin_alignment_analysis_process {
    shift
 
    ORG=` echo ${ORGANISM} | tr [:lower:] [:upper:]  `
-   if [! "${ORG}" == "HOMO_SAPIENS" ]; then
+   if [ ! "${ORG}" == "HOMO_SAPIENS" ]; then
         dieUponError "Invalid organism ${ORG}"
+   fi
+
+      if [ ! "${PLUGINS_ALIGNMENT_ANALYSIS_MUTECT_SOMATIC_MUTATIONS_COVARIATE_INFO_URL}" == "NONE" ]; then
+	    echo "Fetching covariates' details at ${PLUGINS_ALIGNMENT_ANALYSIS_MUTECT_SOMATIC_MUTATIONS_COVARIATE_INFO_URL}"
+        ${RESOURCES_FETCH_URL_SCRIPT} ${PLUGINS_ALIGNMENT_ANALYSIS_MUTECT_SOMATIC_MUTATIONS_COVARIATE_INFO_URL} covariates.tsv ${JOB_DIR}/results/
+   else
+        echo "ERROR: covariates URL not specified"
+   fi
+
+   #only the first part will unzip
+   if [ ! -e ${JOB_DIR}/dbsnp_132_b37.leftAligned.vcf ]; then
+        gunzip ${JOB_DIR}/dbsnp_132_b37.leftAligned.vcf.gz
    fi
 
    BUILD_NUMBER=`echo ${GENOME_REFERENCE_ID} | awk -F\. '{print $1}' | tr [:lower:] [:upper:] `
@@ -48,20 +62,18 @@ function plugin_alignment_analysis_process {
    INDEXED_GENOME_DIR=$(eval echo \${RESOURCES_ARTIFACTS_FAI_INDEXED_GENOMES_SAMTOOLS_FAI_INDEX_${ORG}_${BUILD_NUMBER}_${ENSEMBL_RELEASE}})
 
    WINDOW_LIMITS=`awk -v arrayJobIndex=${ARRAY_JOB_INDEX} '{ if (lineNumber==arrayJobIndex) print " -s "$3" -e "$6; lineNumber++; }' ${SLICING_PLAN_FILENAME}`
-   echo "Fetching covariates' details at ${PLUGINS_ALIGNMENT_ANALYSIS_MUTECT_COVARIATE_INFO_URL}"
-   ${RESOURCES_FETCH_URL_SCRIPT} ${PLUGINS_ALIGNMENT_ANALYSIS_MUTECT_COVARIATE_INFO_URL} covariates.tsv ${JOB_DIR}/results/
-   echo "Fetching b37_cosmic_v54_120711.vcf "
-   ${RESOURCES_FETCH_URL_SCRIPT} http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/b37_cosmic_v54_120711.vcf b37_cosmic_v54_120711.vcf ${JOB_DIR}/
-   echo "Fetching dbsnp_132_b37.leftAligned.vcf.gz "
-   ${RESOURCES_FETCH_URL_SCRIPT} http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/dbsnp_132_b37.leftAligned.vcf.gz dbsnp_132_b37.leftAligned.vcf.gz ${JOB_DIR}/
 
-   gunzip ${JOB_DIR}/dbsnp_132_b37.leftAligned.vcf.gz
+   ls -l
+   pwd
+   tail -n +2 covariates.tsv > covariates_no_header.tsv
 
-   tail -n +2 covariates.tsv > ${TMPDIR}/covariates_no_header.tsv
+   ls -l 
+
+   cat covariates_no_header.tsv
 
    declare -a GermlineDetails='()'
    declare -a SomaticDetails='()'
-
+   
    #loads the samples' details and checks if they exist
     while IFS=$'\t' read sample patient gender type kind tissue parents
     do
@@ -69,7 +81,7 @@ function plugin_alignment_analysis_process {
         kind_lc=`echo ${kind} | tr [:upper:] [:lower:]`
         case "${kind_lc}" in
                 "germline")
-                        FILES=(`ls source/${sample}.*`)
+                        FILES=(`ls ${JOB_DIR}/source/${sample}.*`)
                         if [ ${#FILES[@]} -gt 0 ]; then
                             GermlineDetails["id-${patient}"]="$sample"
                         else
@@ -77,7 +89,7 @@ function plugin_alignment_analysis_process {
                         fi
                         ;;
                 "somatic")
-                         FILES=(`ls source/${sample}.*`)
+                         FILES=(`ls ${JOB_DIR}/source/${sample}.*`)
                          if [ ${#FILES[@]} -gt 0 ]; then
                             SomaticDetails["id-${patient}"]="$sample"
                           else
@@ -87,7 +99,7 @@ function plugin_alignment_analysis_process {
                 *)
                         echo "ERROR: Invalid kind of sample found ${kind} for patient ${patient}"
         esac
-    done < ${TMPDIR}/covariates_no_header.tsv
+    done < covariates_no_header.tsv
 
     #for each pair...
     for id in "${!GermlineDetails[@]}"
@@ -98,19 +110,22 @@ function plugin_alignment_analysis_process {
 
                #1) concatenate-alignments and produce a slice of Goby Alignments (GA) + add-read-origin-info to GA
                echo "Concatenating and slicing germline sample ${GermlineDetails[id]} "
-               run-goby concatenate-alignments \
+               run-goby ${PLUGIN_NEED_PROCESS_JVM} concatenate-alignments \
                     ${WINDOW_LIMITS} \
-                    ---add-read-origin-info
+                    --add-read-origin-info \
                     --output ${TMPDIR}/germline-ca-${GermlineDetails[id]} \
-                    ${TMPDIR}/${GermlineDetails[id]}.entries
-
+                    ${JOB_DIR}/source/${GermlineDetails[id]}.entries
+	           dieUponError "Concatenate of goby alignment for germline sample of ${id}, failed, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
+	
+		
                echo "Concatenating and slicing somatic sample ${SomaticDetails[id]} "
-               run-goby concatenate-alignments \
+               run-goby ${PLUGIN_NEED_PROCESS_JVM} concatenate-alignments \
                     ${WINDOW_LIMITS} \
-                    ---add-read-origin-info
+                    --add-read-origin-info \
                     --output ${TMPDIR}/somatic-ca-${SomaticDetails[id]} \
-                    ${TMPDIR}/${SomaticDetails[id]}.entries
-
+                    ${JOB_DIR}/source/${SomaticDetails[id]}.entries
+               dieUponError "Concatenate of goby alignment for somatic sample of ${id}, failed, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
+	
                #2)convert the GA to BAM
 
                #convert the Germline slice
@@ -119,6 +134,7 @@ function plugin_alignment_analysis_process {
                     --genome ${SEQUENCE_CACHE_DIR}/random-access-genome \
                     --output ${TMPDIR}/germline-ca-${GermlineDetails[id]}.bam \
                     ${TMPDIR}/germline-ca-${GermlineDetails[id]}
+                dieUponError "Convertion of goby alignment to BAM for germline sample of ${id}, failed, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
 
                #convert the Somatic slice
                echo "Converting Goby alignment ${TMPDIR}/somatic-ca-${SomaticDetails[id]} to BAM"
@@ -126,22 +142,25 @@ function plugin_alignment_analysis_process {
                     --genome ${SEQUENCE_CACHE_DIR}/random-access-genome \
                     --output ${TMPDIR}/somatic-ca-${SomaticDetails[id]}.bam \
                     ${TMPDIR}/somatic-ca-${SomaticDetails[id]}
+                dieUponError "Convertion of goby alignment to BAM  for somatic sample of ${id}, failed, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
+
 
                #3) run MuTect
                echo "Running MuTect with \
                  --input_file:normal ${TMPDIR}/germline-ca-${GermlineDetails[id]}.bam  \
                  --input_file:tumor ${TMPDIR}/somatic-ca-${SomaticDetails[id]}.bam  \
                  --out ${id}-stats.tsv"
-               ${MUTECT_EXEC_PATH} \
+               ${RESOURCES_MUTECT_EXEC_PATH} \
                     --analysis_type MuTect \
                     --input_file:normal ${TMPDIR}/germline-ca-${GermlineDetails[id]}.bam  \
                     --input_file:tumor ${TMPDIR}/somatic-ca-${SomaticDetails[id]}.bam \
                     --out ${id}-stats.tsv  \
-                    --reference_sequence ${INDEXED_GENOME_DIR}/*toplevel.fasta
-                    --cosmic ${JOBDIR}/b37_cosmic_v54_120711.vcf
-                    --dbsnp ${JOBDIR}/dbsnp_132_b37.leftAligned.vcf
+                    --reference_sequence ${INDEXED_GENOME_DIR}/*toplevel.fasta \
+                    --cosmic ${JOB_DIR}/b37_cosmic_v54_120711.vcf \
+                    --dbsnp ${JOB_DIR}/dbsnp_132_b37.leftAligned.vcf
                     #--intervals <intervals_to_process>  #may need to convert the WINDOW_LIMITS to mutect syntax
                     #--coverage_file <coverage.wig.txt>
+                 dieUponError "MuTect analysis on  ${id}, failed, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
 
 
         else
@@ -160,7 +179,8 @@ function plugin_alignment_analysis_combine {
     # use goby in fdr mode. To concatenate TSVs produced by Mutect in a single file
     echo "Concating TSVs with Goby in results.tsv"
     run-goby ${PLUGIN_NEED_COMBINE_JVM} fdr \
-        --output results.tsv \
+        --output ${JOBDIR}/results/results.tsv \
         *-stats.tsv
+     dieUponError "Concatenation of TSV files with Goby failed" 
 
 }
