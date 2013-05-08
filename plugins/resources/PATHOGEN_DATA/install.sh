dae6e3667cd1864980fd4a3cc79b8ed968e21351
Manuele Simi <manuele.simi@campagnelab.org>
2013-05-08 02:39:51 +0000

    Add microref and viralref to Pathogen DB (still need to add LAST). Copy the generated references in the installation path.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/campagnelab/gobyweb/trunk/gobyweb_home/conf@34297 60cd2ccc-2b05-0410-9566-da273a940bff

--- plugins/resources/PATHOGEN_DATA/install.sh
+++ plugins/resources/PATHOGEN_DATA/install.sh
@@ -31,6 +31,11 @@ function plugin_install_artifact {
 
             tar -cvzf fungalref.tar.gz fungalref*
 
+            mkdir -p "${installation_path}/fungal"
+            cp fungalref.tar.gz "${installation_path}/fungal"
+            cp fungal-names.map "${installation_path}/fungal"
+
+            rm  -rf *.*
 
             return 0
             ;;
@@ -41,13 +46,59 @@ function plugin_install_artifact {
             ${JOB_DIR}/fetch_url_pattern "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/microbial/" "microbial.*.rna.fna.gz"
             ${JOB_DIR}/fetch_url_pattern "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/microbial/" "microbial.*.genomic.fna.gz"
 
+            #Concatenate them
+            cat *.fna.gz > joined.fa.gz
+            #Extract them to one big fasta file
+            cat *.fna.gz > micro.all.fna.gz
+            gunzip micro.all.fna.gz
+            #Grep fasta file for lines starting with '>' and write the output to another file
+            grep -a ">" micro.all.fna > micro.all.names.map
+            #Cut the first character from each of those lines (the >)
+            cat micro.all.names.map | cut -c2- > micro.all.names.edited.map
+            awk '{print "\t"$0}' micro.all.names.edited.map
+            awk '{print $1"\t"$1=""; print $0}' micro.all.names.edited.map  | more
+            awk '{$1=$1"\t"; print $0}' micro.all.names.edited.map  > micro-names.map
+            lastdb -Q 0 -v microref micro.all.rna.fna
+
+            tar -cvzf microref.tar.gz microref*
+
+            mkdir -p "${installation_path}/bacterial"
+            cp microref.tar.gz "${installation_path}/bacterial"
+            cp micro-names.map "${installation_path}/bacterial"
+
+            rm  -rf *.*
+
+            return 0
             ;;
         'VIRAL' )
               #${RESOURCES_FETCH_URL_SCRIPT_PATTERN} "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/" "viral.*.rna.fna.gz"
             #${RESOURCES_FETCH_URL_SCRIPT_PATTERN} "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/" "viral.*.rna.fna.gz"
             ${JOB_DIR}/fetch_url_pattern "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/" "viral.*.rna.fna.gz"
             ${JOB_DIR}/fetch_url_pattern "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/" "viral.*.genomic.fna.gz"
+            echo working dir `pwd`
+            #Concatenate them
+            cat *.fna.gz > joined.fa.gz
+            #Extract them to one big fasta file
+            cat *.fna.gz > viral.all.fna.gz
+            gunzip viral.all.fna.gz
+            #Grep fasta file for lines starting with '>' and write the output to another file
+            grep -a ">" viral.all.fna > viral.all.names.map
+            #Cut the first character from each of those lines (the >)
+            cat viral.all.names.map | cut -c2- > viral.all.names.edited.map
+            awk '{print "\t"$0}' viral.all.names.edited.map
+            awk '{print $1"\t"$1=""; print $0}' viral.all.names.edited.map  | more
+            awk '{$1=$1"\t"; print $0}' viral.all.names.edited.map  > viral-names.map
+            lastdb -Q 0 -v viralref viral.all.rna.fna
 
+            tar -cvzf viralref.tar.gz viral*
+
+            mkdir -p "${installation_path}/viral"
+            cp viralref.tar.gz "${installation_path}/viral"
+            cp viral-names.map "${installation_path}/viral"
+
+            rm  -rf *.*
+
+            return 0
             ;;
 
         *)  echo "Resource artifact id not recognized: "+$id
