25638153e6950d6919c536aaa0319cef6010d998
Manuele Simi <manuele.simi@campagnelab.org>
2013-05-08 16:11:44 +0000

    CONTAMINANT_EXTRACT: add dependency on PATHOGEN_DATA, change the analysis script to consume the files produced by the resource's artifacts.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/campagnelab/gobyweb/trunk/gobyweb_home/conf@34300 60cd2ccc-2b05-0410-9566-da273a940bff

--- plugins/analyses/CONTAMINANT_EXTRACT/script.sh
+++ plugins/analyses/CONTAMINANT_EXTRACT/script.sh
@@ -106,15 +106,18 @@ function plugin_alignment_analysis_process {
 
 	if [ "${PLUGINS_ALIGNMENT_ANALYSIS_CONTAMINANT_EXTRACT_SEARCH_REFERENCE}" == "VIRAL" ]; then
         #extract viral ref tarball
-        tar -zxvf ${NODE_LOCAL_DATA_ROOT}../pathogen-db/viral/viralref.tar.gz
+        #tar -zxvf ${NODE_LOCAL_DATA_ROOT}../pathogen-db/viral/viralref.tar.gz
+        tar -zxvf ${RESOURCES_ARTIFACTS_PATHOGEN_DATA_VIRAL}/viral/viralref.tar.gz
         local REF_BASENAME="viralref"
     elif [ "${PLUGINS_ALIGNMENT_ANALYSIS_CONTAMINANT_EXTRACT_SEARCH_REFERENCE}" == "MICROBIAL" ]; then
         #extract microbial ref tarball
-        tar -zxvf ${NODE_LOCAL_DATA_ROOT}../pathogen-db/bacterial/microref.tar.gz
+        #tar -zxvf ${NODE_LOCAL_DATA_ROOT}../pathogen-db/bacterial/microref.tar.gz
+        tar -zxvf ${RESOURCES_ARTIFACTS_PATHOGEN_DATA_MICROBIAL}/bacterial/microref.tar.gz
         local REF_BASENAME="microref"
     else
          #extract fungal ref tarball
-        tar -zxvf ${NODE_LOCAL_DATA_ROOT}../pathogen-db/fungal/fungalref.tar.gz
+        #tar -zxvf ${NODE_LOCAL_DATA_ROOT}../pathogen-db/fungal/fungalref.tar.gz
+        tar -zxvf ${RESOURCES_ARTIFACTS_PATHOGEN_DATA_FUNGI}/fungal/fungalref.tar.gz
         local REF_BASENAME="fungalref"
     fi
 
@@ -205,11 +208,14 @@ function plugin_alignment_analysis_combine {
 	dieUponError "Could not combine realigned output files"
 	
 	if [ "${PLUGINS_ALIGNMENT_ANALYSIS_CONTAMINANT_EXTRACT_SEARCH_REFERENCE}" == "VIRAL" ]; then
-        ACCESSION_NAME_MAP="${NODE_LOCAL_DATA_ROOT}../pathogen-db/viral/viral-names.map"
+        #ACCESSION_NAME_MAP="${NODE_LOCAL_DATA_ROOT}../pathogen-db/viral/viral-names.map"
+        ACCESSION_NAME_MAP="${RESOURCES_ARTIFACTS_PATHOGEN_DATA_VIRAL}/viral/viral-names.map"
     elif [ "${PLUGINS_ALIGNMENT_ANALYSIS_CONTAMINANT_EXTRACT_SEARCH_REFERENCE}" == "MICROBIAL" ]; then
-        ACCESSION_NAME_MAP="${NODE_LOCAL_DATA_ROOT}../pathogen-db/bacterial/micro-names.map"
+        #ACCESSION_NAME_MAP="${NODE_LOCAL_DATA_ROOT}../pathogen-db/bacterial/micro-names.map"
+        ACCESSION_NAME_MAP="${RESOURCES_ARTIFACTS_PATHOGEN_DATA_MICROBIAL}/bacterial/micro-names.map"
     else
-        ACCESSION_NAME_MAP="${NODE_LOCAL_DATA_ROOT}../pathogen-db/fungal/fungal-names.map"
+        #ACCESSION_NAME_MAP="${NODE_LOCAL_DATA_ROOT}../pathogen-db/fungal/fungal-names.map"
+        ACCESSION_NAME_MAP="${RESOURCES_ARTIFACTS_PATHOGEN_DATA_FUNGI}/fungal/fungal-names.map"
     fi
 
 	
