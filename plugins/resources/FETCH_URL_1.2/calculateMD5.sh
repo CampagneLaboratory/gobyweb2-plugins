5876c5d880a1c295ec5b9b06d657e49488781628
Manuele Simi <manuele.simi@campagnelab.org>
2013-05-09 15:34:31 +0000

    Move the error message in the correct branch of the if statement when calculating the MD5.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/campagnelab/gobyweb/trunk/gobyweb_home/conf@34312 60cd2ccc-2b05-0410-9566-da273a940bff

--- plugins/resources/FETCH_URL_1.2/calculateMD5.sh
+++ plugins/resources/FETCH_URL_1.2/calculateMD5.sh
@@ -6,10 +6,11 @@ function calculateMD5 {
  #the following should work on linux
  if [[ -x `which md5sum` ]]; then
      RESULT=`echo ${md5url} | md5sum |cut -d " " -f 1`
-
  #the following should work on Mac OS X
  elif [[ -x `which md5` ]]; then
      RESULT=`echo ${md5url} | md5`
+ else
+    echo "MD5 cannot be calculated for ${md5url}"
  fi
- echo "MD5 cannot be calculated for ${md5url}"
+
 }
