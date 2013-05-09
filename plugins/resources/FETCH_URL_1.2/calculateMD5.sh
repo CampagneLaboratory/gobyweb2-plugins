fce504e2859f51fce2c31ae11bf868035eaa8d74
Manuele Simi <manuele.simi@campagnelab.org>
2013-05-09 14:30:11 +0000

    Uses an if/elif statement in calculate MD5 to avoid an error message at each execution if the first condition matches.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/campagnelab/gobyweb/trunk/gobyweb_home/conf@34311 60cd2ccc-2b05-0410-9566-da273a940bff

--- plugins/resources/FETCH_URL_1.2/calculateMD5.sh
+++ plugins/resources/FETCH_URL_1.2/calculateMD5.sh
@@ -6,11 +6,10 @@ function calculateMD5 {
  #the following should work on linux
  if [[ -x `which md5sum` ]]; then
      RESULT=`echo ${md5url} | md5sum |cut -d " " -f 1`
- fi
 
  #the following should work on Mac OS X
- if [[ -x `which md5` ]]; then
+ elif [[ -x `which md5` ]]; then
      RESULT=`echo ${md5url} | md5`
  fi
-
+ echo "MD5 cannot be calculated for ${md5url}"
 }
