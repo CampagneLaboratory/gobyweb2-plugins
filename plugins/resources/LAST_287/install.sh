ec6820874e8a6fd567e50d152b67f566841c6382
Fabien Campagne <fac2003@campagnelab.org>
2013-05-28 11:24:39 -0400

    Upgrade to Last 287. Note the new URL to download Last archive directly from the mercurial repo as a zip file.

--- plugins/resources/LAST_287/install.sh
+++ plugins/resources/LAST_287/install.sh
@@ -9,12 +9,13 @@ function plugin_install_artifact {
         'BINARIES' )
 
                 (
-                VERSION="286"
-                    ${RESOURCES_FETCH_URL_SCRIPT} http://last.cbrc.jp/archive/last-${VERSION}.zip
+                VERSION="287"
+                    ${RESOURCES_FETCH_URL_SCRIPT} http://last.cbrc.jp/last/index.cgi/archive/${VERSION}.zip
 
                     unzip last-${VERSION}.zip
 
                     cd  last-${VERSION}
+                    echo "${VERSION}" > src/version.hh
                     make
                     mkdir ${installation_path}/bin/
                     cp src/lastdb ${installation_path}/bin/
