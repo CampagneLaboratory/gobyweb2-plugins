00df306ef788a149fa0c08abb7cbff8215070f3c
Manuele Simi <manuele.simi@campagnelab.org>
2013-08-22 17:25:12 -0400

    Add MPS resource to install MPS 3.0 RC3 libraries and NYoSh support libraries. So far, it just installs MPS libs and wildcard lib.

---
+++ plugins/resources/MPS_129.302/install.sh
@@ -0,0 +1,35 @@
+# Installation script for MPS 3.0 EAP
+function plugin_install_artifact {
+
+    id=$1
+    installation_path=$2
+
+    case ${id} in
+
+        'DISTRIBUTION' )
+            VERSION="3.0"
+            BUILD="RC3-129.302"
+            ${RESOURCES_FETCH_URL_SCRIPT} http://download.jetbrains.com/mps/30/MPS-${VERSION}-${BUILD}.tar.gz MPS-${VERSION}-${BUILD}.tar.gz
+            gzip -c -d MPS-${VERSION}-${BUILD}.tar.gz |tar -xvf -
+            cp -r MPS\ ${VERSION}/* ${installation_path}/
+            return 0
+            ;;
+         'SUPPORT_LIBS' )
+                download_wildcard
+                return 0;
+            ;;
+        *)  echo "Resource artifact id not recognized: "+$id
+            return 99
+            ;;
+
+    esac
+
+    exit 1
+
+}
+
+function download_wildcard {
+   ${RESOURCES_FETCH_URL_SCRIPT} http://wildcard.googlecode.com/files/wildcard-1.03.zip wildcard-1.03.zip
+   unzip wildcard-1.03.zip
+   cp wildcard-1.03/wildcard-1.03.jar ${installation_path}/
+}
