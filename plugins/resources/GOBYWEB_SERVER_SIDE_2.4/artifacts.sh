451f68f9eb29d4929b21505c2dfecedd8fc675d2
Fabien Campagne <fac2003@campagnelab.org>
2014-03-23 14:49:45 -0400

    Print artifact errors only when the artifact installation failed.

--- plugins/resources/GOBYWEB_SERVER_SIDE_2.4/artifacts.sh
+++ plugins/resources/GOBYWEB_SERVER_SIDE_2.4/artifacts.sh
@@ -13,7 +13,7 @@ function install_plugin_artifacts {
 
 function install_plugin_artifacts_internal {
     if [ -e ${JOB_DIR}/artifacts-install-requests.pb ]; then
-       set +xv
+       set -xv
        . ${JOB_DIR}/constants.sh
        . ${JOB_DIR}/auto-options.sh
        local installation_type=$1
@@ -27,10 +27,15 @@ function install_plugin_artifacts_internal {
                 --ssh-requests  ${JOB_DIR}/artifacts-install-requests.pb \
                 --install --installation-type ${installation_type} --log-dir ${TMPDIR}/steplogs
        local STATUS=$?
-       java -Xms40m -Xmx250m -cp ${JOB_DIR}/goby/serverside-dependencies.jar::${JOB_DIR}/goby/stepslogger.jar \
-                              org.campagnelab.stepslogger.StepsLogTool \
-                              --action view \
-                              ${TMPDIR}/steplogs/*
+       if [ ${STATUS} != 0 ]; then
+           echo "Unable to install artifacts. Aborting"
+           java -Xms40m -Xmx250m -cp ${JOB_DIR}/goby/serverside-dependencies.jar::${JOB_DIR}/goby/stepslogger.jar \
+                               org.campagnelab.stepslogger.StepsLogTool \
+                               --action view \
+                               ${TMPDIR}/steplogs/*
+           exit 100
+       fi
+
        if [ ${STATUS} != 0 ]; then
 
             ${QUEUE_WRITER} --tag ${TAG} --status ${JOB_PART_FAILED_STATUS} --description "Job failed: unable to install required software/data artifacts." --index ${CURRENT_PART} --job-type job
