9f5d0b54cd00a86aebd3cfff8203de80ee10f468
Fabien Campagne <fac2003@campagnelab.org>
2014-05-01 10:36:49 -0400

    plast and LAST_INDEX: Since we get the node in exclusive, and we adjust how much memory we use to index the db according to how much is on the node, we don’t need to request a minimum anymore. This makes it possible to run last on each node of the cluster, rather than the subset with more than 42GB of total memory. Consequently, remove OGE minimum memory requirement for job to start.

--- plugins/resources/LAST_INDEX_456/install.sh
+++ plugins/resources/LAST_INDEX_456/install.sh
@@ -20,7 +20,8 @@ function plugin_install_artifact {
             NUM_THREADS=`grep physical  /proc/cpuinfo |grep id|wc -l`
 
             INPUT_FASTA_NO_GZ=${FAI_INDEXED_GENOME_DIR}/genome-toplevel.fasta
-            ${LASTDB} -s 25G index ${INPUT_FASTA_NO_GZ}
+            VOLUME_SIZE_OPTION=`cat /proc/meminfo |grep MemTotal|awk '{print "-s "($2*3/4)"K"}'`
+            ${LASTDB} ${VOLUME_SIZE_OPTION} index ${INPUT_FASTA_NO_GZ}
             if [ $? != 0 ]; then
                return 1;
             fi
