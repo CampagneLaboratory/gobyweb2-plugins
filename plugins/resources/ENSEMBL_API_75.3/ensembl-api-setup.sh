c30ccdcab937cb31523bd7ce1b52931e32086ce9
Fabien Campagne <fac2003@campagnelab.org>
2014-05-09 19:08:07 -0400

    We need to fetch the version of the VEP_CACHE that matches the ensembl API version.

--- plugins/resources/ENSEMBL_API_75.3/ensembl-api-setup.sh
+++ plugins/resources/ENSEMBL_API_75.3/ensembl-api-setup.sh
@@ -4,5 +4,5 @@ PERL5LIB=${PERL5LIB}:${installation_path}/src/bioperl-1.2.3
 PERL5LIB=${PERL5LIB}:${installation_path}/src/ensembl/modules
 PERL5LIB=${PERL5LIB}:${installation_path}/src/ensembl-compara/modules
 PERL5LIB=${PERL5LIB}:${installation_path}/src/ensembl-variation/modules
-PERL5LIB=${PERL5LIB}:${installation_path}/src/ensembl-funcgen/modules
+PERL5LIB=${PERL5LIB}:${installation_path}/src/ensembl-functgenomics/modules
 export PERL5LIB
