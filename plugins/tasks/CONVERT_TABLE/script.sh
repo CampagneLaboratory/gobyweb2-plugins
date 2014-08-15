87ee12f8dddc9bed66b5549f003cdebacc0f495f
Fabien Campagne <fac2003@campagnelab.org>
2014-08-15 11:19:01 -0400

    Add draft of CONVERT_TABLE task plugin.

---
+++ plugins/tasks/CONVERT_TABLE/script.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+#
+. ${JOB_DIR}/constants.sh
+
+READ_FILES_LIST=""
+
+function plugin_task {
+     set -xv
+     cd ${TMPDIR}
+     echo "fileset command: ${FILESET_COMMAND}"
+
+     ${FILESET_COMMAND} --has-fileset INPUT_VCF
+     if [ ! $? -eq 0 ]; then
+
+        VCF_INPUT=`${FILESET_COMMAND} --fetch INPUT_VCF`
+        dieUponError "Failed to fetch input vcf files ${VCF_INPUT}"
+        echo "${VCF_INPUT}"
+
+        export QUEUE_WRITER
+        ${RESOURCES_GROOVY_EXECUTABLE} -cp ${GOBY_DIR}:${RESOURCES_GOBYWEB_SERVER_SIDE_GLOBAL_GOBY_JAR}:${RESOURCES_GOBYWEB_SERVER_SIDE_ICB_GROOVY_SUPPORT_JAR} \
+        ${RESOURCES_GOBYWEB_SERVER_SIDE_TSV_VCF_TO_SQLITE} \
+        --job-start-status "${JOB_START_STATUS}" \
+        --queue-writer-prefix-variable QUEUE_WRITER \
+        --export-format lucene   "${VCF_INPUT}"
+        jobDieUponError "failed to convert VCF results to Lucene Table"
+     fi
+
+     ${FILESET_COMMAND} --has-fileset INPUT_TSV
+     if [ ! $? -eq 0 ]; then
+
+        TSV_INPUT=`${FILESET_COMMAND} --fetch INPUT_TSV`
+        dieUponError "Failed to fetch input tsv files ${TSV_INPUT}"
+        echo "${TSV_INPUT}"
+
+        export QUEUE_WRITER
+        ${RESOURCES_GROOVY_EXECUTABLE} -cp ${GOBY_DIR}:${RESOURCES_GOBYWEB_SERVER_SIDE_GLOBAL_GOBY_JAR}:${RESOURCES_GOBYWEB_SERVER_SIDE_ICB_GROOVY_SUPPORT_JAR} \
+        ${RESOURCES_GOBYWEB_SERVER_SIDE_TSV_VCF_TO_SQLITE} \
+        --job-start-status "${JOB_START_STATUS}" \
+        --queue-writer-prefix-variable QUEUE_WRITER \
+        --export-format lucene  "${TSV_INPUT}"
+        jobDieUponError "failed to convert TSV results to Lucene Table"
+     fi
+
+     REGISTERED_TAGS=`${FILESET_COMMAND} --push LUCENE_TABLE:  `
+     dieUponError "Failed to push back the Lucene Table Result"
+
+     ALL_REGISTERED_TAGS="${REGISTERED_TAGS} "
+     echo "The following tags were registered by this plugin: ${ALL_REGISTERED_TAGS}"
+     exit $STATUS
+}
+
+
