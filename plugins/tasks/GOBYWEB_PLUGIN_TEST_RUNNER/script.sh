0af117b8f8f79cfea65ee9f495db4f38bdbf7f8e
Manuele Simi <manuele.simi@campagnelab.org>
2014-11-16 10:57:50 -0500

    Fix variable name for test classes.

--- plugins/tasks/GOBYWEB_PLUGIN_TEST_RUNNER/script.sh
+++ plugins/tasks/GOBYWEB_PLUGIN_TEST_RUNNER/script.sh
@@ -5,12 +5,12 @@ function plugin_task {
     ${FILESET_COMMAND} --has-fileset TEST_CLASSES
     dieUponError "Input test classes are not available"
 
-    FILES_WITH_LIST=`${FILESET_COMMAND} --fetch TEST_CLASSES`
-    dieUponError "Failed to fetch test classes files ${TEST_CLASSES}"
-    echo ${TEST_CLASSES}
+    TEST_CLASSES_JAR=`${FILESET_COMMAND} --fetch TEST_CLASSES`
+    dieUponError "Failed to fetch test classes files ${TEST_CLASSES_JAR}"
+    echo ${TEST_CLASSES_JAR}
 
     mkdir ./source
-    cp $FILES_WITH_LIST ./source/JarWithTests.jar
+    cp "${TEST_CLASSES_JAR}" ./source/JarWithTests.jar
     rm -rf ./additionalTests/
     mvn clean
     mvn test
