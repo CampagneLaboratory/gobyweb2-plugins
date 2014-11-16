6591640740a7b3a92ebc8824dbd2be0d69143090
Manuele Simi <manuele.simi@campagnelab.org>
2014-11-16 10:56:14 -0500

    Fetch test classes with FileSet command and move the jar in the position expected by the pom.

--- plugins/tasks/GOBYWEB_PLUGIN_TEST_RUNNER/script.sh
+++ plugins/tasks/GOBYWEB_PLUGIN_TEST_RUNNER/script.sh
@@ -2,6 +2,16 @@
 
 
 function plugin_task {
+    ${FILESET_COMMAND} --has-fileset TEST_CLASSES
+    dieUponError "Input test classes are not available"
+
+    FILES_WITH_LIST=`${FILESET_COMMAND} --fetch TEST_CLASSES`
+    dieUponError "Failed to fetch test classes files ${TEST_CLASSES}"
+    echo ${TEST_CLASSES}
+
+    mkdir ./source
+    cp $FILES_WITH_LIST ./source/JarWithTests.jar
+    rm -rf ./additionalTests/
     mvn clean
     mvn test
     # Surefire report directory will be: ./target/plugins-reports
