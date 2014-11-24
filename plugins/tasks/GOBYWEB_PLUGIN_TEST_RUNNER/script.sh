938ab5288450d8405afdf368dda7ee792351efea
Manuele Simi <manuele.simi@campagnelab.org>
2014-11-24 17:17:06 -0500

    Update Runner jar.

--- plugins/tasks/GOBYWEB_PLUGIN_TEST_RUNNER/script.sh
+++ plugins/tasks/GOBYWEB_PLUGIN_TEST_RUNNER/script.sh
@@ -1,21 +1,21 @@
 
-export M2_HOME="${RESOURCES_ARTIFACTS_MAVEN_DISTRIBUTION}"
-export MAVEN_OPTS="-Xms512m -Xmx1g"
+    export M2_HOME="${RESOURCES_ARTIFACTS_MAVEN_DISTRIBUTION}"
+    export MAVEN_OPTS="-Xms512m -Xmx1g"
 
-function plugin_task {
-    ${FILESET_COMMAND} --has-fileset TEST_CLASSES
-    dieUponError "Input test classes are not available"
+    function plugin_task {
+        ${FILESET_COMMAND} --has-fileset TEST_CLASSES
+        dieUponError "Input test classes are not available"
 
-    TEST_CLASSES_JAR=`${FILESET_COMMAND} --fetch TEST_CLASSES`
-    dieUponError "Failed to fetch test classes files ${TEST_CLASSES_JAR}"
-    echo ${TEST_CLASSES_JAR}
-    mkdir ${JOB_DIR}/source
-    cp "${TEST_CLASSES_JAR}" ${JOB_DIR}/source/JarWithTests.jar
-    rm -rf ./additionalTests/
-    ${RESOURCES_ARTIFACTS_MAVEN_DISTRIBUTION}/bin/mvn -f ${JOB_DIR}/pom.xml clean
-    ${RESOURCES_ARTIFACTS_MAVEN_DISTRIBUTION}/bin/mvn -f ${JOB_DIR}/pom.xml process-test-resources
-    ${RESOURCES_ARTIFACTS_MAVEN_DISTRIBUTION}/bin/mvn -f ${JOB_DIR}/pom.xml surefire:test -Dtest=${PLUGINS_TASK_GOBYWEB_PLUGIN_TEST_RUNNER_TEST_NAMES}
-    # Surefire report directory will be: ./target/plugins-reports
-    REPORT=`cat ${JOB_DIR}/target/plugins-reports/*.txt`
-    info "${REPORT}" 'post_process'
-}
+        TEST_CLASSES_JAR=`${FILESET_COMMAND} --fetch TEST_CLASSES`
+        dieUponError "Failed to fetch test classes files ${TEST_CLASSES_JAR}"
+        echo ${TEST_CLASSES_JAR}
+        mkdir ${JOB_DIR}/source
+        cp "${TEST_CLASSES_JAR}" ${JOB_DIR}/source/JarWithTests.jar
+        rm -rf ./additionalTests/
+        ${RESOURCES_ARTIFACTS_MAVEN_DISTRIBUTION}/bin/mvn -f ${JOB_DIR}/pom.xml clean
+        ${RESOURCES_ARTIFACTS_MAVEN_DISTRIBUTION}/bin/mvn -f ${JOB_DIR}/pom.xml process-test-resources
+        ${RESOURCES_ARTIFACTS_MAVEN_DISTRIBUTION}/bin/mvn -f ${JOB_DIR}/pom.xml surefire:test -Dtest=${PLUGINS_TASK_GOBYWEB_PLUGIN_TEST_RUNNER_TEST_NAMES}
+        # Surefire report directory will be: ./target/plugins-reports
+        REPORT=`cat ${JOB_DIR}/target/plugins-reports/*.txt`
+        info "${REPORT}" 'post_process'
+    }
