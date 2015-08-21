8250fbdcf5a6d84ae18754d4f67e47b246128a89
Fabien Campagne <fac2003@campagnelab.org>
2015-08-21 11:43:09 -0400

    Preliminary support for CUSTOM reference as .fa.gz files.

--- plugins/resources/ENSEMBL_GENOMES_1.0/install.sh
+++ plugins/resources/ENSEMBL_GENOMES_1.0/install.sh
@@ -15,13 +15,18 @@ function plugin_install_artifact {
             ORG_LOWERCASE=`echo  ${ORGANISM}| tr '[:upper:]' '[:lower:]'`
             # get genome from local NFS, or wget from ensembl servers if not present
             BUILD_NUMBER=`echo ${GENOME_REFERENCE_ID} | awk -F\. '{print $1}'`
-            if [ "${BUILD_NUMBER}" = "GRCh37" -o "${BUILD_NUMBER}" = "1000GENOMES" ]; then
-                # For human, use the compatible 1000g assembly instead of the Ensembl build: (coordinates are compatible),
-                # see http://www.1000genomes.org/category/assembly
-                ${RESOURCES_FETCH_URL_SCRIPT} ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/human_g1k_v37.fasta.gz human.dna.toplevel.fa.gz
-            else
-                ${RESOURCES_FETCH_URL_SCRIPT_PATTERN} "ftp://ftp.ensembl.org/pub/release-${ENSEMBL_RELEASE}/fasta/${ORG_LOWERCASE}/dna/" ".dna.toplevel.fa.gz"
 
+            if [ "${BUILD_NUMBER}" = "CUSTOM" ]; then
+                install_custom_fasta ${GENOME_REFERENCE_ID}
+            else
+                if [ "${BUILD_NUMBER}" = "GRCh37" -o "${BUILD_NUMBER}" = "1000GENOMES" ]; then
+                    # For human, use the compatible 1000g assembly instead of the Ensembl build: (coordinates are compatible),
+                    # see http://www.1000genomes.org/category/assembly
+                    ${RESOURCES_FETCH_URL_SCRIPT} ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/human_g1k_v37.fasta.gz human.dna.toplevel.fa.gz
+                else
+                    ${RESOURCES_FETCH_URL_SCRIPT_PATTERN} "ftp://ftp.ensembl.org/pub/release-${ENSEMBL_RELEASE}/fasta/${ORG_LOWERCASE}/dna/" ".dna.toplevel.fa.gz"
+
+                fi
             fi
             cp *.dna.toplevel.fa.gz ${installation_path}/genome-toplevel.fasta.gz
             if [ -e ${installation_path}/genome-toplevel.fasta.gz ]; then
@@ -42,7 +47,13 @@ function plugin_install_artifact {
     exit 1
 
 }
-
+function install_custom_fasta() {
+   id=$1
+   FASTA_FILE_TAG=`echo ${GENOME_REFERENCE_ID} | awk -F\. '{print $(NF)}'`
+   # TODO: use fileset --fetch with tag to retrieve the file (to be implemented in the plugins-sdk command line).
+   # TODO: For now, just copy from a fixed directory.
+   cp ~gobyweb/tmp/custom-ref-${FASTA_FILE_TAG}.fa.gz custom-${FASTA_FILE_TAG}.dna.toplevel.fa.gz
+}
 
 function get_attribute_values() {
 
