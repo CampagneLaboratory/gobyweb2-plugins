df9fabd1676d1aac9d20997ed11cba493bbe03ee
Manuele Simi <manuele.simi@campagnelab.org>
2015-08-24 15:58:50 -0400

    Add resource GOBYWEB_SERVER_SIDE 2.5.2. This resource is intended to be used from the gobyweb webapp (the SDK in the gobyweb2.3 branch is built to use this version).

---
+++ plugins/resources/GOBYWEB_SERVER_SIDE_2.5.2/artifact-manager.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+    if [ ! -x "${JOB_DIR}" ]; then
+       echo "You must define JOB_DIR before running this job."
+       exit 1;
+    fi
+    . ${JOB_DIR}/constants.sh;
+    . ${JOB_DIR}/auto-options.sh;
+    mkdir -p ${ARTIFACT_REPOSITORY_DIR}
+    RUN_ARTIFACT_MANAGER=" java -Xms40m -Xmx500m -Dlog4j.configuration=file:${JOB_DIR}/goby/log4j.properties \
+                     -Djava.io.tmpdir=${TMPDIR} \
+                     -cp ${JOB_DIR}/goby/serverside-dependencies.jar:${JOB_DIR}/goby/artifact-manager.jar::${JOB_DIR}/goby/stepslogger.jar \
+                      org.campagnelab.gobyweb.artifacts.ArtifactManager \
+                      --repository ${ARTIFACT_REPOSITORY_DIR} --repo-dir-quota  1000000000 "
+    ${RUN_ARTIFACT_MANAGER} "$@"
