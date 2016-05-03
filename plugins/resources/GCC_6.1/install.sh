536c1a33e0a8057d8df6e961f76394ee26e104a0
REMI TORRACINTA <remi.torracinta@campagnelab.org>
2016-05-03 17:58:36 -0400

    adding gcc 6.1, dependencies working. need to make distclean before making gcc?

--- plugins/resources/GCC_6.1/install.sh
+++ plugins/resources/GCC_6.1/install.sh
@@ -7,40 +7,51 @@ function plugin_install_artifact {
     case ${id} in
 
         'BINARIES' )
-            VERSION="4.8.5"
-            ${RESOURCES_FETCH_URL_SCRIPT} http://ftp.gnu.org/gnu/gcc/gcc-$VERSION/gcc-$VERSION.tar.gz gcc-$VERSION.tar.gz
-            tar -zxvf gcc-$VERSION.tar.gz
+            VERSION="6.1.0"
+            set -x
+            #${RESOURCES_FETCH_URL_SCRIPT} http://mirrors-usa.go-parts.com/gcc/releases/gcc-$VERSION/gcc-$VERSION.tar.gz gcc.tar.gz
+            #${RESOURCES_FETCH_URL_SCRIPT} http://mirrors.concertpass.com/gcc/releases/gcc-$VERSION/gcc-$VERSION.tar.gz gcc.tar.gz
+            ${RESOURCES_FETCH_URL_SCRIPT} file://~/usr/gcc/gcc.tar.gz gcc.tar.gz
+            tar -zxvf gcc.tar.gz
             cd gcc-$VERSION
             SOURCE_DIR=$(pwd)
 
             ## from here: script adapted from https://gist.github.com/pmalek/5da2e947a51aa773da80
 
             SOURCE_DIR_NAME=${SOURCE_DIR##*/}
-            cd ${SOURCE_DIR}/contrib/
+            cd ${SOURCE_DIR}/
 
             # download prerequisites for gcc's configure
-            ./download_prerequisites
+            ./contrib/download_prerequisites
 
             # check the downloads worked
-            if [ ! -d gmp ] || [ ! -d mpfr ] || [ ! -d mpc ]; then
+            if [ ! -d gmp ]; then
               echo "There is no gmp or mpfr or mpc directory in $(pwd). Please run download_prerequisites."
+              return 127
+            fi
+            if [ ! -d mpfr ]; then
+              echo "There is no mpfr or mpc directory in $(pwd). Please run download_prerequisites."
+              return 127
+            fi
+            if [ ! -d gmp ]; then
+              echo "There is no mpc directory in $(pwd). Please run download_prerequisites."
               return 127
             fi
 
             # install gmp
-            cd ${SOURCE_DIR}/contrib/gmp
+            cd ${SOURCE_DIR}/gmp
             ./configure --enable-shared --enable-static --prefix=$installation_path && \
             make && make check && make install
             echo "gmp Installed"
 
             # install mpfr
-            cd ${SOURCE_DIR}/contrib/mpfr
+            cd ${SOURCE_DIR}/mpfr
             ./configure --enable-shared --enable-static --prefix=$installation_path --with-gmp=$installation_path && \
             make && make check && make install
             echo "mpfr Installed"
 
             # install mpc
-            cd ${SOURCE_DIR}/contrib/mpc
+            cd ${SOURCE_DIR}/mpc
             ./configure --enable-shared --enable-static --prefix=$installation_path --with-gmp=$installation_path --with-mpfr=$installation_path && \
             make && make check && make install
             echo "mpc Installed"
@@ -53,7 +64,7 @@ function plugin_install_artifact {
             ulimit -s 32768 # for gcc tests
             mkdir -p $installation_path/auto-load/usr/lib
             mkdir -p gcc-build && cd gcc-build
-
+            make distclean
             ${SOURCE_DIR}/configure --enable-shared --prefix=$installation_path  --enable-bootstrap --enable-languages=c,c++ --enable-libgomp --enable-threads=posix --with-gmp=$installation_path --with-mpfr=$installation_path \
              --with-mpc=$installation_path --with-fpmath=sse --disable-multilib
             # force to create sym links instead of hard links (not allowed in the artifact repo mounted on docker)
@@ -73,7 +84,6 @@ function plugin_install_artifact {
             fi
             return 127
             ;;
-
 
         *)  echo "Resource artifact id not recognized: "+$id
             exit 99
