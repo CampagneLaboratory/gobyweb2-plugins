ea61bf27b2cb015698ded4ed7f7f238f45fba547
REMI TORRACINTA <remi.torracinta@campagnelab.org>
2016-05-03 10:24:56 -0400

    cookie alternative attempt

--- plugins/resources/JAVA_8.92/install.sh
+++ plugins/resources/JAVA_8.92/install.sh
@@ -6,12 +6,13 @@ function plugin_install_artifact {
     case ${id} in
 
         'BINARIES' )
-            VERSION="8u92"
-            VERSIONALT="1.80_92"
+            VERSION="8u45"
+            VERSIONALT="1.80_45"
             set -x
-            ${RESOURCES_FETCH_URL_SCRIPT} http://download.oracle.com/otn-pub/java/jdk/$VERSION-b14/jdk-$VERSION-linux-x64.tar.gz java.tar.gz "" "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"
+            ${RESOURCES_FETCH_URL_SCRIPT} http://download.oracle.com/otn-pub/java/jdk/$VERSION-b14/jdk-$VERSION-linux-x64.tar.gz java.tar.gz "" '-v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie"' http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
+#           ${RESOURCES_FETCH_URL_SCRIPT} http://www.vim.org/scripts/download_script.php?src_id=6273 java.tar.gz ""  '"gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;"'
             tar -zxvf java.tar.gz -C /${installation_path}
-            cd jre$VERSIONALT
+            cd jdk$VERSIONALT
             if [ -e ${installation_path}/bin/java ]; then
                return 0
             else
