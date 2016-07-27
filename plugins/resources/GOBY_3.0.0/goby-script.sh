1b1ccaa35d001216d365be98a4b460d0b8a1f1dd
REMI TORRACINTA <remi.torracinta@campagnelab.org>
2016-07-27 11:48:11 -0400

    model copied to goby resource directory, and goby_home exported by run script.

--- plugins/resources/GOBY_3.0.0/goby-script.sh
+++ plugins/resources/GOBY_3.0.0/goby-script.sh
@@ -2,6 +2,7 @@
 function run_goby {
    set -x
    set -T
+   set_home
    memory="$1"
    shift
    mode_name="$1"
@@ -17,6 +18,7 @@ function run_goby {
 function goby {
    set -x
    set -T
+   set_home
    mode_name="$1"
    shift
      # set both minimum and max right away so failure occurs early and parallel GC is happier.
@@ -24,4 +26,14 @@ function goby {
                                              -Dgoby.configuration=file:${TMPDIR}/goby.properties \
                        -jar ${RESOURCES_ARTIFACTS_GOBY_JAR}/goby.jar \
                        --mode ${mode_name} $*
+}
+
+
+function set_home {
+   WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    if [[ $OSTYPE == "cygwin" ]]; then
+        WORKING_DIR=`cygpath -m "${WORKING_DIR}"`
+    fi
+
+    export GOBY_HOME=${WORKING_DIR}
 }
--- plugins/resources/GOBY_3.0.0/install.sh
+++ plugins/resources/GOBY_3.0.0/install.sh
@@ -11,10 +11,12 @@ function plugin_install_artifact {
             VERSION="3.0.0"
 
             git clone git@bitbucket.org:campagnelaboratory/goby.git
-            git checkout tags/3.0.0
             cd goby
+            git checkout tags/3.0.0
             ant -f build.xml jar
             mv goby.jar ${installation_path}
+            mkdir ${installation_path}/models
+            mv /models/* ${installation_path}/models
             chmod +x ${installation_path}/*
             if [ -e ${installation_path}/goby.jar ]; then
                 return 0
