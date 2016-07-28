5f969979b10b6509fc9d998edb1fb0c4fd9e4dbc
Manuele Simi <manuele.simi@campagnelab.org>
2016-07-28 15:11:21 -0400

    Install java in the installation path. Plugins using JAVA should not be aware of the specific release of the JDK downloaded by the JAVA plugin.

--- plugins/resources/JAVA_8.92/install.sh
+++ plugins/resources/JAVA_8.92/install.sh
@@ -12,8 +12,9 @@ function plugin_install_artifact {
             ${RESOURCES_FETCH_URL_SCRIPT} http://download.oracle.com/otn-pub/java/jdk/$VERSION-b14/jdk-$VERSION-linux-x64.tar.gz java.tar.gz "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;oraclelicense=accept-securebackup-cookie"
 #           ${RESOURCES_FETCH_URL_SCRIPT} http://www.vim.org/scripts/download_script.php?src_id=6273 java.tar.gz ""  '"gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;"'
             tar -zxvf java.tar.gz -C ${installation_path}
-            cd ${installation_path}/jdk$VERSIONALT
-            if [ -e ${installation_path}/jdk$VERSIONALT/bin/java ]; then
+            mv ${installation_path}/jdk$VERSIONALT/* ${installation_path}/
+            cd ${installation_path}/
+            if [ -e ${installation_path}/bin/java ]; then
                return 0
             else
                return 127
