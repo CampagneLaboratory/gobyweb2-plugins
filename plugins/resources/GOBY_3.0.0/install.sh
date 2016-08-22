a381e3a2d5576e275dbd70cd82ffec96a79f4898
Manuele Simi <manuele.simi@campagnelab.org>
2016-08-22 13:14:36 -0400

    Do not check out the tagged version. Goby3 is now on master.

--- plugins/resources/GOBY_3.0.0/install.sh
+++ plugins/resources/GOBY_3.0.0/install.sh
@@ -8,10 +8,9 @@ function plugin_install_artifact {
 
         'JAR' )
 
-            VERSION="3.0.0.3"
+            VERSION="3.0.0.4"
             git clone git@bitbucket.org:campagnelaboratory/goby3.git
             cd goby3
-            git checkout tags/${VERSION}
             export JAVA_HOME=${RESOURCES_ARTIFACTS_JAVA_LINUX_BINARIES}
             ant -f build.xml jar
             mv goby.jar ${installation_path}
