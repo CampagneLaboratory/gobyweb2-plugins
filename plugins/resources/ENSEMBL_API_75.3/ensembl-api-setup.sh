357d6f665583025db8f466de625a08cbb2be43c6
mas2182 <manuele.simi@campagnelab.org>
2016-09-04 22:08:07 -0400

    Update bioperl path inside the setup script of ENSEMBL_API (was bioperl-1.2.3, now is BioPerl-1.6.923).

--- plugins/resources/ENSEMBL_API_75.3/ensembl-api-setup.sh
+++ plugins/resources/ENSEMBL_API_75.3/ensembl-api-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 installation_path=${RESOURCES_ARTIFACTS_ENSEMBL_API_INSTALL_DIR}
-PERL5LIB=${PERL5LIB}:${installation_path}/src/bioperl-1.2.3
+PERL5LIB=${PERL5LIB}:${installation_path}/src/BioPerl-1.6.923
 PERL5LIB=${PERL5LIB}:${installation_path}/src/ensembl/modules
 PERL5LIB=${PERL5LIB}:${installation_path}/src/ensembl-compara/modules
 PERL5LIB=${PERL5LIB}:${installation_path}/src/ensembl-variation/modules
