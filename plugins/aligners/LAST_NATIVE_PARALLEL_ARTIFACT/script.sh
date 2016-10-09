b20a0f685278f32f61f95237556a09d1b8587b4c
Fabien Campagne <fac2003@campagnelab.org>
2016-10-09 13:42:47 -0400

    Add draft version of last alignment with native parallelization (new -P option in lastal).

---
+++ plugins/aligners/LAST_NATIVE_PARALLEL_ARTIFACT/script.sh
@@ -0,0 +1,90 @@
+# COPYRIGHT_MAY_GO_HERE
+# This script expects the following variables to be defined:
+
+# PAIRED_END_ALIGNMENT = true|false
+# COLOR_SPACE = true|false
+# READS = reads file
+# START_POSITION = start index in the reads file
+# END_POSITION = end index in the reads file
+
+# INDEX_DIRECTORY = directory that contains the indexed database
+# INDEX_PREFIX = name of the indexed database to search
+
+# ALIGNER_OPTIONS = any Last options the end-user would like to set
+
+# Please note that Goby must be configured with appropriate path to Last aligner executable.
+. ${RESOURCES_GOBY_SHELL_SCRIPT}
+
+# override the number of alignment parts to make larger chunks:
+NUMBER_OF_ALIGN_PARTS=100
+
+function plugin_align {
+
+      OUTPUT=$1
+      BASENAME=$2
+
+      COLOR_SPACE_OPTION=""
+      if [ "${COLOR_SPACE}" == "true" ]; then
+          COLOR_SPACE_OPTION=" --color-space "
+      fi
+
+        NUM_THREADS=`grep physical  /proc/cpuinfo |grep id|wc -l`
+        NUM_THREADS=$((${NUM_THREADS} - 2))
+        if [ ${NUM_THREADS} -lt 2 ]; then
+           # Make sure we use at least two threads:
+           NUM_THREADS=2
+        fi
+
+
+    ORG=` echo ${ORGANISM} | tr [:lower:] [:upper:]  `
+    BUILD_NUMBER=`echo ${GENOME_REFERENCE_ID} | awk -F\. '{print $1}' | tr [:lower:] [:upper:] `
+    ENSEMBL_RELEASE=`echo ${GENOME_REFERENCE_ID} | awk -F\. '{print $(NF)}'| tr [:lower:] [:upper:] `
+
+    INDEX_DIRECTORY=$(eval echo \${RESOURCES_ARTIFACTS_LAST_INDEX_INDEX_${ORG}_${BUILD_NUMBER}_${ENSEMBL_RELEASE}}/)
+    TOPLEVEL_DIRECTORY=$(eval echo \${RESOURCES_ARTIFACTS_LAST_INDEX_TOPLEVEL_IDS_${ORG}_${BUILD_NUMBER}_${ENSEMBL_RELEASE}}/)
+
+    #make sure index exists
+    if [ ! -e ${INDEX_DIRECTORY}/index.prj ]; then
+        failThisLine
+      #	dieUponError "last index could not be found"
+    fi
+       set -x
+      # Extract the reads if a split is needed
+      if [ ! -z ${SGE_TASK_ID} ] && [ "${SGE_TASK_ID}" != "undefined" ] && [ "${SGE_TASK_ID}" != "unknown" ]; then
+          ${QUEUE_WRITER} --tag ${TAG} --status ${JOB_PART_SPLIT_STATUS} --description "Split, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, starting" --index ${CURRENT_PART} --job-type job-part
+          # The reads file to process
+          READS_FILE=${READS##*/}
+
+          ls -l ${READS_FILE}
+          ls -l ${READS}
+          run_goby ${PLUGIN_NEED_ALIGN_JVM} reformat-compact-reads --output ${READS_FILE} \
+              --start-position ${START_POSITION} --end-position ${END_POSITION} ${READS}
+
+          dieUponError "split reads failed, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
+      fi
+    TEMP_FILENAME="last-alignment"
+    ${RESOURCES_LAST_EXEC_PATH} -P ${NUM_THREADS} -s2 -Q1 -d${PLUGINS_ALIGNER_PLAST_ARTIFACT_D} \
+        -e${PLUGINS_ALIGNER_PLAST_ARTIFACT_E} ${INDEX_DIRECTORY}/index ${READS_FASTQ} > ${TEMP_FILENAME}.maf
+    dieUponError "last could not align reads"
+    if [ "${PAIRED_END_ALIGNMENT}" == "true" ]; then
+        ${RESOURCES_LAST_EXEC_PATH} -P ${NUM_THREADS} -s2 -Q1 -d${PLUGINS_ALIGNER_PLAST_ARTIFACT_D} \
+           -e${PLUGINS_ALIGNER_PLAST_ARTIFACT_E} ${INDEX_DIRECTORY}/index ${PAIRS_FASTQ} > ${TEMP_FILENAME}-pairs.maf
+        dieUponError "last could not align paired reads"
+        ${RESOURCES_ARTIFACTS_LAST_ARTIFACT_BINARIES}/scripts/last-pair-probs.py ${TEMP_FILENAME}.maf ${TEMP_FILENAME}-pairs.maf > ${TEMP_FILENAME}-2.maf
+        if [ $? != 0 ]; then
+           cat ${TEMP_FILENAME}.maf ${TEMP_FILENAME}-pairs.maf > ${TEMP_FILENAME}-2.maf
+        fi
+        dieUponError "last could not last-pair-probs.py"
+    else
+        cat ${TEMP_FILENAME}.maf | ${RESOURCES_LAST_MAP_PROBS_EXEC} -s${PLUGINS_ALIGNER_PLAST_ARTIFACT_S} > ${TEMP_FILENAME}-2.maf
+        dieUponError "last could not last-map-probs.py"
+    fi
+    REFERENCE=${TOPLEVEL_DIRECTORY}/toplevel-ids.compact-reads
+
+    java -Xmx${PLUGIN_NEED_ALIGN_JVM} -Dlog4j.debug=true -Dlog4j.configuration=file:${JOB_DIR}/goby/log4j.properties \
+                        -Dgoby.configuration=file:${TMPDIR}/goby.properties \
+                        -jar ${RESOURCES_GOBY_GOBY_JAR} \
+                        --mode last-to-compact -i ${TEMP_FILENAME}-2.maf -o ${OUTPUT} --third-party-input true \
+                        --only-maf -q ${FULL_READS_INPUT} -t ${REFERENCE} --quality-filter-parameters threshold=1.0
+     dieUponError "Aligning forward and reverse strand results failed. Goby could not convert maf to compact alignment format, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
+}
---
+++ plugins/resources/LAST_759/install.sh
@@ -0,0 +1,47 @@
+# Installation script for STAR version 2.2.0g
+function plugin_install_artifact {
+
+    id=$1
+    installation_path=$2
+
+    case ${id} in
+
+        'BINARIES' )
+
+                (
+                VERSION="759"
+                    ${RESOURCES_FETCH_URL_SCRIPT} http://last.cbrc.jp/last/index.cgi/archive/${VERSION}.zip ${VERSION}.zip
+                    unzip ${VERSION}.zip
+
+                    cd  last-${VERSION}
+                    echo "${VERSION}" > src/version.hh
+                    make
+                    mkdir ${installation_path}/bin/
+                    cp src/lastdb ${installation_path}/bin/
+                    cp src/lastal ${installation_path}/bin/
+                    cp -r scripts ${installation_path}/
+                    cp -r examples ${installation_path}/
+                    # older versions of last defined scripts with the .sh or .py extension. Create links to
+                    # maintain backward compatibility:
+                    cd ${installation_path}/scripts/
+                    ln -s  last-map-probs last-map-probs.py
+                )
+            if [ -e ${installation_path}/bin/lastal ]; then
+               return 0
+            else
+               return 127
+            fi
+            return 127
+            ;;
+
+
+
+        *)  echo "Resource artifact id not recognized: "+$id
+            exit 99
+            ;;
+
+    esac
+
+    exit 1
+
+}
