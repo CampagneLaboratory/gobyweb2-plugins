d92dd03af69d6329b2de150d7034a2d5c7d9a0fe
Manuele Simi <manuele.simi@campagnelab.org>
2016-10-13 11:10:41 -0400

    Restore obsolete plugin goby 3.0.0. It is needed to access to jobs that used that version from GobyWeb.

---
+++ plugins/resources/GOBY_3.0.0/install.sh
@@ -0,0 +1,32 @@
+# Installation script for GOBY version 3.0.0
+function plugin_install_artifact {
+
+    id=$1
+    installation_path=$2
+
+    case ${id} in
+
+        'JAR' )
+
+            git clone git@bitbucket.org:campagnelaboratory/goby3.git
+            cd goby3
+            export JAVA_HOME=${RESOURCES_ARTIFACTS_JAVA_LINUX_BINARIES}
+            mvn install
+            mv goby.jar ${installation_path}
+            mkdir ${installation_path}/models
+            mv ./models/* ${installation_path}/models
+            chmod +x ${installation_path}/*
+            if [ -e ${installation_path}/goby.jar ]; then
+                return 0
+            else
+               return 127
+            fi
+            ;;
+
+        *)  echo "Resource artifact id not recognized: "+$id
+            exit 99
+            ;;
+
+    esac
+
+    exit 1
