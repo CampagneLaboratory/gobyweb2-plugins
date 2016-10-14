f05b49dff5b799b8a07aa8b2cc5112f93268fb81
Fabien Campagne <fac2003@campagnelab.org>
2016-10-14 14:47:43 -0400

    goby3: Configure slf4j.

--- plugins/resources/GOBY3_SNAPSHOT/goby3-script.sh
+++ plugins/resources/GOBY3_SNAPSHOT/goby3-script.sh
@@ -8,9 +8,10 @@ function run_goby {
    shift
    mode_name="$1"
    shift
+   SLF4J_CONFIG=${RESOURCES_ARTIFACTS_GOBY3_JAR}/config/goby-logback.xml
    # set both minimum and max right away so failure occurs early and parallel GC is happier.
    ${RESOURCES_ARTIFACTS_JAVA_LINUX_BINARIES}/bin/java -Xms${memory} -Xmx${memory} ${JVM_MEM_OPTIONS} \
-                       -Dlog4j.debug=true -Dlog4j.configuration=file:${TMPDIR}/log4j.properties \
+                       -Dlogback.configurationFile=${SLF4J_CONFIG} \
                        -Dgoby.configuration=file:${TMPDIR}/goby.properties \
                        -jar ${RESOURCES_ARTIFACTS_GOBY3_JAR}/goby.jar \
                        --mode ${mode_name} $*
@@ -23,10 +24,11 @@ function goby {
    set_home
    mode_name="$1"
    shift
+      SLF4J_CONFIG=${RESOURCES_ARTIFACTS_GOBY3_JAR}/config/goby-logback.xml
      # set both minimum and max right away so failure occurs early and parallel GC is happier.
    ${RESOURCES_ARTIFACTS_JAVA_LINUX_BINARIES}/bin/java ${GRID_JVM_FLAGS} ${JVM_MEM_OPTIONS} \
-                        -Dlog4j.debug=true -Dlog4j.configuration=file:${TMPDIR}/log4j.properties \
-                        -Dgoby.configuration=file:${TMPDIR}/goby.properties \
+                       -Dlogback.configurationFile=${SLF4J_CONFIG} \
+                       -Dgoby.configuration=file:${TMPDIR}/goby.properties \
                        -jar ${RESOURCES_ARTIFACTS_GOBY3_JAR}/goby.jar \
                        --mode ${mode_name} $*
 }
