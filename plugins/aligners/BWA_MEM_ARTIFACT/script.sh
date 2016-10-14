a2baf8a5f37d24d92a1a8ca1a52e9a9cdb31c183
Fabien Campagne <fac2003@campagnelab.org>
2016-10-14 13:59:32 -0400

    Source run-goby script to activate Goby3.

--- plugins/aligners/BWA_MEM_ARTIFACT/script.sh
+++ plugins/aligners/BWA_MEM_ARTIFACT/script.sh
@@ -63,7 +63,7 @@ function plugin_align {
         dieUponError "SE alignment failed, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
     fi
     set -x
-    echo    GENOME_DIR=$(eval echo \${RESOURCES_ARTIFACTS_GOBY_INDEXED_GENOMES_SEQUENCE_CACHE_${ORGANISM}_${BUILD_NUMBER}_${ENSEMBL_RELEASE}})
-    run_goby ${PLUGIN_NEED_ALIGN_JVM} sam-to-compact -i output.sam -o ${OUTPUT} #--genome GENOME_DIR/index
+    export GENOME_DIR=$(eval echo \${RESOURCES_ARTIFACTS_GOBY_INDEXED_GENOMES_SEQUENCE_CACHE_${ORGANISM}_${BUILD_NUMBER}_${ENSEMBL_RELEASE}})
+    run_goby ${PLUGIN_NEED_ALIGN_JVM} sam-to-compact -i output.sam -o ${OUTPUT} --genome ${GENOME_DIR}/index
     dieUponError "SAM conversion to Goby fomat failed, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
 }
