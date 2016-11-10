dcd2b62d12f00531802597b946f879a10778d9eb
Fabien Campagne <fac2003@campagnelab.org>
2016-11-09 23:52:59 -0500

    Adjust result file paths.

--- plugins/analyses/SEQUENCE_BASE_INFORMATION/script.sh
+++ plugins/analyses/SEQUENCE_BASE_INFORMATION/script.sh
@@ -135,12 +135,14 @@ function plugin_alignment_analysis_combine {
    RECORDS_PER_BUCKET=${PLUGINS_ALIGNMENT_ANALYSIS_SEQUENCE_BASE_INFORMATION_RECORDS_PER_BUCKET}
    ${RESOURCES_ARTIFACTS_JAVA_LINUX_BINARIES}/bin/java -cp ${RESOURCES_ARTIFACTS_DLVARIATION_JAR}/model-training-bin.jar  -Xmx${PLUGIN_NEED_COMBINE_JVM}  \
                                         org.campagnelab.dl.varanalysis.tools.Randomize \
-                                        -i  ${JOB_DIR}/split-mutated/*.sbi -o ${TMPDIR}/${TAG}-mutated-out \
+                                        -i  ${JOB_DIR}/split-mutated/*.sbi -o ${TMPDIR}/${TAG}-mutated-randomized \
                                         --records-per-bucket ${RECORDS_PER_BUCKET} --chunk-size 50
 
-   mkdir -p ${JOB_DIR}/results
-   cp ${TMPDIR}/${TAG}-out.sbi* ${RESULT_DIR}/
-   cp ${TMPDIR}/${TAG}-mutated-out* ${RESULT_DIR}/
+   cp ${TMPDIR}/${TAG}-mutated-randomized.sbi ./mutated-randomized.sbi
+   cp ${TMPDIR}/${TAG}-mutated-randomized.sbip ./mutated-randomized.sbip
+  # mkdir -p ${JOB_DIR}/results
+  # cp ${TMPDIR}/${TAG}-out.sbi* ${RESULT_DIR}/${TAG}
+  # cp ${TMPDIR}/${TAG}-mutated-randomized* ${RESULT_DIR}/${TAG}
    # Make a backup of the results in case the web app fails to display (https://bitbucket.org/campagnelaboratory/gobyweb/issues/20/alignment-analysis-job-completed-but-error):
    mkdir ${JOB_DIR}/results-copy
    cp  ${TMPDIR}/${TAG}-out.sbi* ${TMPDIR}/${TAG}-mutated-out*  ${JOB_DIR}/results-copy
