065e06dc8d4734f6ec054c5cfd47f9541ca94da6
Manuele Simi <manuele.simi@campagnelab.org>
2017-02-01 16:15:13 -0500

    Add GOBY3_INDELS_SNAPSHOT resource. This snapshot is built from the mapIndels branch.

---
+++ plugins/resources/GOBY3_INDELS_SNAPSHOT/goby3-script.sh
@@ -0,0 +1,41 @@
+JVM_MEM_OPTIONS="-XX:+UseCompressedClassPointers -XX:MaxMetaspaceSize=512m -XX:CompressedClassSpaceSize=512m"
+# COPYRIGHT_MAY_GO_HERE
+function run_goby {
+   set -x
+   set -T
+   set_home
+   memory="$1"
+   shift
+   mode_name="$1"
+   shift
+   SLF4J_CONFIG=${RESOURCES_ARTIFACTS_GOBY3_JAR}/config/goby-logback.xml
+   # set both minimum and max right away so failure occurs early and parallel GC is happier.
+   ${RESOURCES_ARTIFACTS_JAVA_LINUX_BINARIES}/bin/java -Xms${memory} -Xmx${memory} ${JVM_MEM_OPTIONS} \
+                       -Djava.ext.dirs=${RESOURCES_ARTIFACTS_GOBY3_JAR}/ \
+                       -Dlogback.configurationFile=${SLF4J_CONFIG} \
+                       -Dgoby.configuration=file:${TMPDIR}/goby.properties \
+                       -jar ${RESOURCES_ARTIFACTS_GOBY3_JAR}/goby.jar \
+                       --mode ${mode_name} $*
+}
+
+
+function goby3 {
+   set -x
+   set -T
+   set_home
+   mode_name="$1"
+   shift
+      SLF4J_CONFIG=${RESOURCES_ARTIFACTS_GOBY3_JAR}/config/goby-logback.xml
+     # set both minimum and max right away so failure occurs early and parallel GC is happier.
+   ${RESOURCES_ARTIFACTS_JAVA_LINUX_BINARIES}/bin/java ${GRID_JVM_FLAGS} ${JVM_MEM_OPTIONS} \
+                       -Djava.ext.dirs=${RESOURCES_ARTIFACTS_GOBY3_JAR}/ \
+                       -Dlogback.configurationFile=${SLF4J_CONFIG} \
+                       -Dgoby.configuration=file:${TMPDIR}/goby.properties \
+                       -jar ${RESOURCES_ARTIFACTS_GOBY3_JAR}/goby.jar \
+                       --mode ${mode_name} $*
+}
+
+
+function set_home {
+    export GOBY_HOME=${RESOURCES_ARTIFACTS_GOBY3_JAR}
+}
---
+++ plugins/resources/GOBY3_INDELS_SNAPSHOT/install.sh
@@ -0,0 +1,31 @@
+# Installation script for GOBY version 3.0.0
+function plugin_install_artifact {
+
+    id=$1
+    installation_path=$2
+
+    case ${id} in
+
+        'JAR' )
+            VERSION="3.2.1-SNAPSHOT"
+            ${RESOURCES_FETCH_URL_SCRIPT} https://www.dropbox.com/s/n4p7vwovz6mdrl1/goby-${VERSION}.zip
+            unzip goby-${VERSION}.zip
+            export JAVA_HOME=${RESOURCES_ARTIFACTS_JAVA_LINUX_BINARIES}
+            mv goby-${VERSION}/* ${installation_path}
+            chmod +x ${installation_path}/goby
+            if [ -e ${installation_path}/goby.jar ]; then
+                return 0
+            else
+               return 127
+            fi
+            ;;
+
+        *)  echo "Resource artifact id not recognized: "+$id
+            exit 99
+            ;;
+
+    esac
+
+    exit 1
+
+}
