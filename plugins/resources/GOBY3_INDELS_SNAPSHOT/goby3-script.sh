6eeb0fdddc13e6cb9d139f040daff1cfd3dbb1eb
Manuele Simi <manuele.simi@campagnelab.org>
2017-02-01 22:45:06 -0500

    Fix GOBY3_INDELS variables inside the script.

--- plugins/resources/GOBY3_INDELS_SNAPSHOT/goby3-script.sh
+++ plugins/resources/GOBY3_INDELS_SNAPSHOT/goby3-script.sh
@@ -8,13 +8,13 @@ function run_goby {
    shift
    mode_name="$1"
    shift
-   SLF4J_CONFIG=${RESOURCES_ARTIFACTS_GOBY3_JAR}/config/goby-logback.xml
+   SLF4J_CONFIG=${RESOURCES_ARTIFACTS_GOBY3_INDELS_JAR}/config/goby-logback.xml
    # set both minimum and max right away so failure occurs early and parallel GC is happier.
    ${RESOURCES_ARTIFACTS_JAVA_LINUX_BINARIES}/bin/java -Xms${memory} -Xmx${memory} ${JVM_MEM_OPTIONS} \
-                       -Djava.ext.dirs=${RESOURCES_ARTIFACTS_GOBY3_JAR}/ \
+                       -Djava.ext.dirs=${RESOURCES_ARTIFACTS_GOBY3_INDELS_JAR}/ \
                        -Dlogback.configurationFile=${SLF4J_CONFIG} \
                        -Dgoby.configuration=file:${TMPDIR}/goby.properties \
-                       -jar ${RESOURCES_ARTIFACTS_GOBY3_JAR}/goby.jar \
+                       -jar ${RESOURCES_ARTIFACTS_GOBY3_INDELS_JAR}/goby.jar \
                        --mode ${mode_name} $*
 }
 
@@ -25,17 +25,17 @@ function goby3 {
    set_home
    mode_name="$1"
    shift
-      SLF4J_CONFIG=${RESOURCES_ARTIFACTS_GOBY3_JAR}/config/goby-logback.xml
+      SLF4J_CONFIG=${RESOURCES_ARTIFACTS_GOBY3_INDELS_JAR}/config/goby-logback.xml
      # set both minimum and max right away so failure occurs early and parallel GC is happier.
    ${RESOURCES_ARTIFACTS_JAVA_LINUX_BINARIES}/bin/java ${GRID_JVM_FLAGS} ${JVM_MEM_OPTIONS} \
-                       -Djava.ext.dirs=${RESOURCES_ARTIFACTS_GOBY3_JAR}/ \
+                       -Djava.ext.dirs=${RESOURCES_ARTIFACTS_GOBY3_INDELS_JAR}/ \
                        -Dlogback.configurationFile=${SLF4J_CONFIG} \
                        -Dgoby.configuration=file:${TMPDIR}/goby.properties \
-                       -jar ${RESOURCES_ARTIFACTS_GOBY3_JAR}/goby.jar \
+                       -jar ${RESOURCES_ARTIFACTS_GOBY3_INDELS_JAR}/goby.jar \
                        --mode ${mode_name} $*
 }
 
 
 function set_home {
-    export GOBY_HOME=${RESOURCES_ARTIFACTS_GOBY3_JAR}
+    export GOBY_HOME=${RESOURCES_ARTIFACTS_GOBY3_INDELS_JAR}
 }
