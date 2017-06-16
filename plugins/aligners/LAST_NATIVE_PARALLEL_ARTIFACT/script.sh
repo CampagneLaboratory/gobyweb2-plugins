20d9e1ed0b479e51fcd9fdbc8a885ec3588bb2c1
Fabien Campagne <fac2003@campagnelab.org>
2017-06-16 15:37:19 -0400

    More work on parallel native last aligner script.

--- plugins/aligners/LAST_NATIVE_PARALLEL_ARTIFACT/script.sh
+++ plugins/aligners/LAST_NATIVE_PARALLEL_ARTIFACT/script.sh
@@ -13,7 +13,7 @@
 # ALIGNER_OPTIONS = any Last options the end-user would like to set
 
 # Please note that Goby must be configured with appropriate path to Last aligner executable.
-. ${RESOURCES_GOBY_SHELL_SCRIPT}
+. ${RESOURCES_GOBY3_SHELL_SCRIPT}
 
 # override the number of alignment parts to make larger chunks:
 NUMBER_OF_ALIGN_PARTS=`echo $(( NUMBER_OF_ALIGN_PARTS / 20 ))`
@@ -62,32 +62,45 @@ function plugin_align {
           ls -l ${READS}
           run_goby ${PLUGIN_NEED_ALIGN_JVM} reformat-compact-reads --output ${READS_FILE} \
               --start-position ${START_POSITION} --end-position ${END_POSITION} ${READS}
-
           dieUponError "split reads failed, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
       fi
-    TEMP_FILENAME="last-alignment"
-    ${RESOURCES_ARTIFACTS_LAST_ARTIFACT_BINARIES}/bin/lastal -P ${NUM_THREADS} -s2 -Q1 -d${PLUGINS_ALIGNER_LAST_NATIVE_PARALLEL_ARTIFACT_D} \
-        -e${PLUGINS_ALIGNER_LAST_NATIVE_PARALLEL_ARTIFACT_E} ${INDEX_DIRECTORY}/index ${READS_FASTQ} > ${TEMP_FILENAME}.maf
-    dieUponError "last could not align reads"
+    TEMP_FILENAME="last-alignment-${RANDOM}"
+
     if [ "${PAIRED_END_ALIGNMENT}" == "true" ]; then
-        ${RESOURCES_ARTIFACTS_LAST_ARTIFACT_BINARIES}//bin/lastal -P ${NUM_THREADS} -s2 -Q1 -d${PLUGINS_ALIGNER_LAST_NATIVE_PARALLEL_ARTIFACT_D} \
-           -e${PLUGINS_ALIGNER_LAST_NATIVE_PARALLEL_ARTIFACT_E} ${INDEX_DIRECTORY}/index ${PAIRS_FASTQ} > ${TEMP_FILENAME}-pairs.maf
-        dieUponError "last could not align paired reads"
-        ${RESOURCES_ARTIFACTS_LAST_ARTIFACT_BINARIES}/bin/last-pair-probs ${TEMP_FILENAME}.maf ${TEMP_FILENAME}-pairs.maf > ${TEMP_FILENAME}-2.maf
-        if [ $? != 0 ]; then
-           cat ${TEMP_FILENAME}.maf ${TEMP_FILENAME}-pairs.maf > ${TEMP_FILENAME}-2.maf
-        fi
-        dieUponError "last could not last-pair-probs"
+
+        run_goby ${PLUGIN_NEED_ALIGN_JVM} compact-to-fasta -i ${READS} -t fastq \
+            --start-position ${START_POSITION} --end-position ${END_POSITION}  \
+            -o read1.fq -p read2.fq
+        dieUponError "compact read to fastq failed (paired end), sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
     else
-        cat ${TEMP_FILENAME}.maf |  ${RESOURCES_ARTIFACTS_LAST_ARTIFACT_BINARIES}/bin/last-map-probs -s${PLUGINS_ALIGNER_LAST_NATIVE_PARALLEL_ARTIFACT_S} > ${TEMP_FILENAME}-2.maf
-        dieUponError "last could not last-map-probs"
+        run_goby ${PLUGIN_NEED_ALIGN_JVM} compact-to-fasta -i ${READS} -t fastq \
+            --start-position ${START_POSITION} --end-position ${END_POSITION}  \
+            -o read1.fq
+        dieUponError "compact read to fastq failed (single end), sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
+
     fi
+     ${RESOURCES_ARTIFACTS_LAST_ARTIFACT_BINARIES}/bin/lastal -P ${NUM_THREADS} -s2 -Q1 -d${PLUGINS_ALIGNER_LAST_NATIVE_PARALLEL_ARTIFACT_D} \
+        -e${PLUGINS_ALIGNER_LAST_NATIVE_PARALLEL_ARTIFACT_E} ${INDEX_DIRECTORY}/index read1.fq > ${TEMP_FILENAME}.maf
+    dieUponError "last could not align reads"
+
+    if [ "${PAIRED_END_ALIGNMENT}" == "true" ]; then
+            ${RESOURCES_ARTIFACTS_LAST_ARTIFACT_BINARIES}//bin/lastal -P ${NUM_THREADS} -s2 -Q1 -d${PLUGINS_ALIGNER_LAST_NATIVE_PARALLEL_ARTIFACT_D} \
+               -e${PLUGINS_ALIGNER_LAST_NATIVE_PARALLEL_ARTIFACT_E} ${INDEX_DIRECTORY}/index read2.fq > ${TEMP_FILENAME}-pairs.maf
+            dieUponError "last could not align paired reads"
+            ${RESOURCES_ARTIFACTS_LAST_ARTIFACT_BINARIES}/bin/last-pair-probs ${TEMP_FILENAME}.maf ${TEMP_FILENAME}-pairs.maf > ${TEMP_FILENAME}-2.maf
+            if [ $? != 0 ]; then
+               cat ${TEMP_FILENAME}.maf ${TEMP_FILENAME}-pairs.maf > ${TEMP_FILENAME}-2.maf
+            fi
+            dieUponError "last could not last-pair-probs"
+    fi
+
+
+    cat ${TEMP_FILENAME}.maf |  ${RESOURCES_ARTIFACTS_LAST_ARTIFACT_BINARIES}/bin/last-map-probs -s${PLUGINS_ALIGNER_LAST_NATIVE_PARALLEL_ARTIFACT_S} > ${TEMP_FILENAME}-2.maf
+    dieUponError "last could not last-map-probs"
+
     REFERENCE=${TOPLEVEL_DIRECTORY}/toplevel-ids.compact-reads
 
-    java -Xmx${PLUGIN_NEED_ALIGN_JVM} -Dlog4j.debug=true -Dlog4j.configuration=file:${JOB_DIR}/goby/log4j.properties \
-                        -Dgoby.configuration=file:${TMPDIR}/goby.properties \
-                        -jar ${RESOURCES_GOBY_GOBY_JAR} \
-                        --mode last-to-compact -i ${TEMP_FILENAME}-2.maf -o ${OUTPUT} --third-party-input true \
+    run_goby ${PLUGIN_NEED_ALIGN_JVM} last-to-compact -i ${TEMP_FILENAME}-2.maf -o ${OUTPUT} --third-party-input true \
                         --only-maf -q ${READS} -t ${REFERENCE} --quality-filter-parameters threshold=1.0
-     dieUponError "Aligning forward and reverse strand results failed. Goby could not convert maf to compact alignment format, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
+    dieUponError "Aligning forward and reverse strand results failed. Goby could not convert maf to compact alignment format, sub-task ${CURRENT_PART} of ${NUMBER_OF_PARTS}, failed"
 }
