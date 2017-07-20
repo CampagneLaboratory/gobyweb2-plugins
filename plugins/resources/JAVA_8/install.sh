ee300a092b8fff96545fa7fcf81cbc2aba3a8c98
Manuele Simi <manuele.simi@campagnelab.org>
2017-07-20 15:00:44 -0400

    Update java_8 to u141 (previous jdk update is no longer available to download).

--- plugins/resources/JAVA_8/install.sh
+++ plugins/resources/JAVA_8/install.sh
@@ -6,11 +6,10 @@ function plugin_install_artifact {
     case ${id} in
 
         'LINUX_BINARIES' )
-            VERSION="8u45"
-            VERSIONALT="1.8.0_45"
+            VERSION="8u141"
+            VERSIONALT="1.8.0_141"
             set -x
-            ${RESOURCES_FETCH_URL_SCRIPT} http://download.oracle.com/otn-pub/java/jdk/$VERSION-b14/jdk-$VERSION-linux-x64.tar.gz java.tar.gz "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;oraclelicense=accept-securebackup-cookie"
-#           ${RESOURCES_FETCH_URL_SCRIPT} http://www.vim.org/scripts/download_script.php?src_id=6273 java.tar.gz ""  '"gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;"'
+            ${RESOURCES_FETCH_URL_SCRIPT} http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-${VERSION}-linux-x64.tar.gz "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;oraclelicense=accept-securebackup-cookie"
             tar -zxvf java.tar.gz -C ${installation_path}
             mv ${installation_path}/jdk$VERSIONALT/* ${installation_path}/
             cd ${installation_path}/
