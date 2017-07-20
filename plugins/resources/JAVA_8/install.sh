d2ba5cc54f4b72fd505509fc5bdc43d4983eea3b
Manuele Simi <manuele.simi@campagnelab.org>
2017-07-20 15:46:06 -0400

    Fix the command to download the java archive.

--- plugins/resources/JAVA_8/install.sh
+++ plugins/resources/JAVA_8/install.sh
@@ -9,7 +9,7 @@ function plugin_install_artifact {
             VERSION="8u141"
             VERSIONALT="1.8.0_141"
             set -x
-            ${RESOURCES_FETCH_URL_SCRIPT} http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-${VERSION}-linux-x64.tar.gz "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;oraclelicense=accept-securebackup-cookie"
+            ${RESOURCES_FETCH_URL_SCRIPT} http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-${VERSION}-linux-x64.tar.gz java.tar.gz "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;oraclelicense=accept-securebackup-cookie"
             tar -zxvf java.tar.gz -C ${installation_path}
             mv ${installation_path}/jdk$VERSIONALT/* ${installation_path}/
             cd ${installation_path}/
