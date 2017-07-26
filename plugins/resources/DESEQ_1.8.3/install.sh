b8e98dda2f81d9fd995ce4aef2da32156028244f
Manuele Simi <manuele.simi@campagnelab.org>
2017-07-26 14:11:47 -0400

    Advance Biobase version to 2.30.0 (https://support.bioconductor.org/p/49962/).

--- plugins/resources/DESEQ_1.8.3/install.sh
+++ plugins/resources/DESEQ_1.8.3/install.sh
@@ -27,8 +27,8 @@ function plugin_install_artifact {
             ${RUN_R} CMD INSTALL BiocGenerics_${VERSION}.tar.gz --library=${installation_path}
 
             # Biobase
-            VERSION="2.16.0"
-            ${RESOURCES_FETCH_URL_SCRIPT} http://www.bioconductor.org/packages/2.10/bioc/src/contrib/Biobase_${VERSION}.tar.gz
+            VERSION="2.30.0"
+            ${RESOURCES_FETCH_URL_SCRIPT} http://www.bioconductor.org/packages/3.0/bioc/src/contrib/Biobase_${VERSION}.tar.gz
             ${RUN_R} CMD INSTALL Biobase_${VERSION}.tar.gz --library=${installation_path}
 
             # locfit
