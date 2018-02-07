3286c914e72c3df301725cbe30c1a29d031e9dbf
Manuele Simi <manuele.simi@campagnelab.org>
2018-02-07 16:02:32 -0500

    Calculate and set the missing VOLUME_SIZE_OPTION information for LAST_INDEX.

--- plugins/resources/LAST_INDEX/install.sh
+++ plugins/resources/LAST_INDEX/install.sh
@@ -20,6 +20,8 @@ function plugin_install_artifact {
             NUM_THREADS=`grep physical  /proc/cpuinfo |grep id|wc -l`
 
             INPUT_FASTA_NO_GZ=${FAI_INDEXED_GENOME_DIR}/genome-toplevel.fasta
+            VOLUME_SIZE_OPTION=`cat /proc/meminfo |grep MemTotal|awk '{print "-s "($2*3/4)"K"}'`
+
             ${LASTDB} ${VOLUME_SIZE_OPTION} -uNEAR -R01  index -P ${NUM_THREADS} ${INPUT_FASTA_NO_GZ}
             if [ $? != 0 ]; then
                return 1;
