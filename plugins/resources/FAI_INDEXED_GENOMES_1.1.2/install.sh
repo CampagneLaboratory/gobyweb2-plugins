8d1a10295222c123fa6451e83e921b6c251978db
Manuele Simi <manuele.simi@campagnelab.org>
2018-03-03 16:01:12 -0500

    Add new version of Last_index and FAI_INDEXED_GENOMES that depend on samtools 1.7.

---
+++ plugins/resources/FAI_INDEXED_GENOMES_1.1.2/install.sh
@@ -0,0 +1,63 @@
+# Installation script for STAR version 2.2.0g
+function plugin_install_artifact {
+
+    id=$1
+    installation_path=$2
+
+    case ${id} in
+
+
+        'SAMTOOLS_FAI_INDEX' )
+            ORG=$3
+            BUILD_NUMBER=$4
+            ENSEMBL_RELEASE=$5
+
+            GENOME_DIR=$(eval echo \${RESOURCES_ARTIFACTS_ENSEMBL_GENOMES_TOPLEVEL_${ORG}_${BUILD_NUMBER}_${ENSEMBL_RELEASE}})
+            set -e
+            # Link to the genome in the indexed directory, then put the index next to the link:
+            if [ -e ${GENOME_DIR}/genome-toplevel.fasta.gz ]; then
+                gzip -c -d ${GENOME_DIR}/genome-toplevel.fasta.gz >${installation_path}/genome-toplevel.fasta || true
+            fi
+            if [ -e ${GENOME_DIR}/genome-toplevel.fasta ]; then
+                cp ${GENOME_DIR}/genome-toplevel.fasta ${installation_path}/genome-toplevel.fasta || true
+            fi
+
+            set +e
+            ${RESOURCES_ARTIFACTS_SAMTOOLS_BINARIES}/samtools faidx ${installation_path}/genome-toplevel.fasta
+
+            if [ -e ${installation_path}/genome-toplevel.fasta.fai ]; then
+                return 0
+            else
+                return 1
+            fi
+            ;;
+
+        *)  echo "Resource artifact id not recognized: "+$id
+            return 99
+            ;;
+
+    esac
+
+    exit 1
+
+}
+
+
+function get_attribute_values() {
+
+    id=$1
+    out=$2
+
+    echo "get_attribute_values for ID=${id}"
+
+    set -xv
+    BUILD_NUMBER=`echo ${GENOME_REFERENCE_ID} | awk -F\. '{print $1}'`
+    ENSEMBL_VERSION_NUMBER=`echo ${GENOME_REFERENCE_ID} | awk -F\. '{print $(NF)}'`
+    echo >>${out} "organism=${ORGANISM}"
+    echo >>${out} "reference-build=${BUILD_NUMBER}"
+    echo >>${out} "ensembl-version-number=${ENSEMBL_VERSION_NUMBER}"
+
+    echo "Printing result from ${out}:"
+    cat ${out}
+    exit 0
+}
